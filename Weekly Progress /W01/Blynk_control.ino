e6be7c319f37d12df61e2de4bdb4297b8ab97704
jovanedu2014-arch <jovan.edu2014@gmail.com>
2026-09-17 07:34:12 -0400

    Update Blynk control settings and motor configurations

--- Weekly Progress /W01/Blynk_control.ino
+++ Weekly Progress /W01/Blynk_control.ino
@@ -1,40 +1,209 @@
-#define BLYNK_TEMPLATE_ID "YOUR_TEMPLATE_ID"
-#define BLYNK_TEMPLATE_NAME "YOUR_TEMPLATE_NAME"
-#define BLYNK_AUTH_TOKEN "YOUR_AUTH_TOKEN"
+#define BLYNK_TEMPLATE_ID   "TMPL2k6VKVGql"
+#define BLYNK_TEMPLATE_NAME "Connection_of_boxing_bots_from_Harish_to_the_bot"
+#define BLYNK_AUTH_TOKEN    "B13_JGf0JzTasIhX2yM_tTiRY2sTFi7p"
 
-#define BLYNK_PRINT Serial
-
+#include <Bluepad32.h>
+#include <ESP32Servo.h>
 #include <WiFi.h>
 #include <BlynkSimpleEsp32.h>
-#include <ESP32Servo.h>
 
-char ssid[] = "YOUR_WIFI_NAME";
-char pass[] = "YOUR_WIFI_PASSWORD";
-char auth[] = BLYNK_AUTH_TOKEN;
+const char* ssid     = "Jov-Fi";
+const char* password = "Jovsecuregate2014";
 
-// ====== Servo Setup ======
-Servo servo1;  // Left shoulder vertical
-Servo servo2;  // Left shoulder horizontal
-Servo servo3;  // Right shoulder vertical
-Servo servo4;  // Right shoulder horizontal
-Servo hipServo;
+const int motor1_enable_pin   = 13;
+const int motor2_enable_pin   = 25;
+const int motor1_forward_pin  = 12;
+const int motor1_backward_pin = 14;
+const int motor2_forward_pin  = 27;
+const int motor2_backward_pin = 26;
+//const int buzzer               = 15;
 
-// Pins
-int servo1Pin = 2;   // V7
-int servo2Pin = 4;   // V6
-int servo3Pin = 5;   // V5
-int servo4Pin = 18;  // V4
-int hipPin    = 19;  // hip (V10/V11 control)
+const int motor_range_max =  200;
+const int motor_range_min = -200;
 
-// Hip angles
-int centerPos  = 90;
-int leftPos    = 0;
-int rightPos   = 180;
-int currentPos = centerPos;
+const int servo1_pin = 2;
+const int servo2_pin = 4;
+const int servo3_pin = 5;
+const int servo4_pin = 18;
+const int servo5_pin = 19;
 
-// Smooth motion routine for hip
-void smoothMove(Servo &servo, int targetPos) {
-  if (targetPos == currentPos) return;
+Servo servo1, servo2, servo3, servo4, servo5;
+
+ControllerPtr myControllers[BP32_MAX_GAMEPADS] = { nullptr };
+bool controllerConnected = false;
+
+int  blynk_j1_x = 127, blynk_j1_y = 127;
+int  blynk_j2_x = 127, blynk_j2_y = 127;
+bool blynk_fwd   = false;
+bool blynk_bwd   = false;
+bool blynk_left  = false;
+bool blynk_right = false;
+
+bool hookInProgress      = false;
+const bool blynkLocked   = true;   // Blynk-only mode, gamepad permanently blocked
+
+void performL1Action() {
+  if (hookInProgress) return;
+  hookInProgress = true;
+  servo1.write(90);  servo2.write(90);
+  servo3.write(90);  servo4.write(90);
+  servo5.write(45);  servo2.write(40);  servo1.write(180);
+  delay(400);
+  servo1.write(90);  servo2.write(90);
+  servo3.write(90);  servo4.write(90);  servo5.write(90);
+  delay(100);
+  hookInProgress = false;
+}
+
+void performR1Action() {
+  if (hookInProgress) return;
+  hookInProgress = true;
+  servo1.write(90);  servo2.write(90);
+  servo3.write(90);  servo4.write(90);  servo5.write(90);
+  delay(100);
+  servo5.write(135); servo3.write(0);   servo4.write(130);
+  delay(400);
+  servo1.write(90);  servo2.write(90);
+  servo3.write(90);  servo4.write(90);  servo5.write(90);
+  delay(100);
+  hookInProgress = false;
+}
+
+BLYNK_WRITE(V0)  { blynk_j1_x  = param.asInt(); }
+BLYNK_WRITE(V1)  { blynk_j1_y  = param.asInt(); }
+BLYNK_WRITE(V2)  { blynk_j2_x  = param.asInt(); }
+BLYNK_WRITE(V3)  { blynk_j2_y  = param.asInt(); }
+BLYNK_WRITE(V4)  { blynk_fwd   = param.asInt(); }
+BLYNK_WRITE(V5)  { blynk_bwd   = param.asInt(); }
+BLYNK_WRITE(V6)  { blynk_left  = param.asInt(); }
+BLYNK_WRITE(V7)  { blynk_right = param.asInt(); }
+BLYNK_WRITE(V8)  { if (param.asInt()) performL1Action(); }
+BLYNK_WRITE(V9)  { if (param.asInt()) performR1Action(); }
+
+void controlMotor(int forwardPin, int backwardPin, int speed) {
+  if (speed > 0) {
+    analogWrite(forwardPin,  speed);
+    analogWrite(backwardPin, 0);
+  } else if (speed < 0) {
+    analogWrite(forwardPin,  0);
+    analogWrite(backwardPin, -speed);
+  } else {
+    analogWrite(forwardPin,  0);
+    analogWrite(backwardPin, 0);
+  }
+}
+
+void stopAllMotorsAndServos() {
+  controlMotor(motor1_forward_pin, motor1_backward_pin, 0);
+  controlMotor(motor2_forward_pin, motor2_backward_pin, 0);
+  digitalWrite(motor1_enable_pin, LOW);
+  digitalWrite(motor2_enable_pin, LOW);
+  servo1.write(90); servo2.write(90); servo3.write(90);
+  servo4.write(90); servo5.write(90);
+}
+
+void applyMovement(bool fwd, bool bwd, bool left, bool right) {
+  if (fwd) {
+    digitalWrite(motor1_enable_pin, HIGH);
+    digitalWrite(motor2_enable_pin, HIGH);
+    controlMotor(motor1_forward_pin, motor1_backward_pin,  motor_range_max);
+    controlMotor(motor2_forward_pin, motor2_backward_pin,  motor_range_max);
+  } else if (bwd) {
+    digitalWrite(motor1_enable_pin, HIGH);
+    digitalWrite(motor2_enable_pin, HIGH);
+    controlMotor(motor1_forward_pin, motor1_backward_pin,  motor_range_min);
+    controlMotor(motor2_forward_pin, motor2_backward_pin,  motor_range_min);
+  } else if (left) {
+    digitalWrite(motor1_enable_pin, HIGH);
+    digitalWrite(motor2_enable_pin, HIGH);
+    controlMotor(motor1_forward_pin, motor1_backward_pin,  motor_range_min);
+    controlMotor(motor2_forward_pin, motor2_backward_pin,  motor_range_max);
+  } else if (right) {
+    digitalWrite(motor1_enable_pin, HIGH);
+    digitalWrite(motor2_enable_pin, HIGH);
+    controlMotor(motor1_forward_pin, motor1_backward_pin,  motor_range_max);
+    controlMotor(motor2_forward_pin, motor2_backward_pin,  motor_range_min);
+  } else {
+    digitalWrite(motor1_enable_pin, LOW);
+    digitalWrite(motor2_enable_pin, LOW);
+    controlMotor(motor1_forward_pin, motor1_backward_pin,  0);
+    controlMotor(motor2_forward_pin, motor2_backward_pin,  0);
+  }
+}
+
+void onConnectedController(ControllerPtr ctl) {
+  if (blynkLocked) return;  // Reject all BT controller connections
+  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+    if (myControllers[i] == nullptr) {
+      myControllers[i] = ctl;
+      controllerConnected = true;
+      break;
+    }
+  }
+  for (int i = 0; i < 2; i++) {
+    digitalWrite(buzzer, HIGH); delay(500);
+    digitalWrite(buzzer, LOW);  delay(500);
+  }
+}
+
+void onDisconnectedController(ControllerPtr ctl) {
+  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+    if (myControllers[i] == ctl) {
+      myControllers[i] = nullptr;
+      controllerConnected = false;
+      stopAllMotorsAndServos();
+      break;
+    }
+  }
+  digitalWrite(buzzer, HIGH); delay(500);
+  digitalWrite(buzzer, LOW);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(motor1_forward_pin,  OUTPUT);
+  pinMode(motor1_backward_pin, OUTPUT);
+  pinMode(motor2_forward_pin,  OUTPUT);
+  pinMode(motor2_backward_pin, OUTPUT);
+  pinMode(motor1_enable_pin,   OUTPUT);
+  pinMode(motor2_enable_pin,   OUTPUT);
+  pinMode(buzzer,              OUTPUT);
+
+  servo1.attach(servo1_pin);
+  servo2.attach(servo2_pin);
+  servo3.attach(servo3_pin);
+  servo4.attach(servo4_pin);
+  servo5.attach(servo5_pin);
+
+  stopAllMotorsAndServos();
+
+  BP32.setup(&onConnectedController, &onDisconnectedController);
+  BP32.forgetBluetoothKeys();
+  BP32.enableVirtualDevice(false);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { delay(500); }
+
+  Blynk.config(BLYNK_AUTH_TOKEN);
+  Blynk.connect();
+}
+
+void loop() {
+  Blynk.run();
+
+  BP32.update();  // Still call to keep BT stack alive, but data is never used
+
+  if (!hookInProgress) {
+    applyMovement(blynk_fwd, blynk_bwd, blynk_left, blynk_right);
+    servo1.write(map(blynk_j1_y, 0,   255, 0, 180));
+    servo2.write(map(blynk_j1_x, 255,   0, 0, 180));
+    servo3.write(map(blynk_j2_y, 255,   0, 0, 180));
+    servo4.write(map(blynk_j2_x, 255,   0, 0, 180));
+  }
+
+  delay(2);
+}  if (targetPos == currentPos) return;
   if (currentPos < targetPos) {
     for (int p = currentPos; p <= targetPos; p++) {
       servo.write(p);
@@ -49,7 +218,7 @@ void smoothMove(Servo &servo, int targetPos) {
   currentPos = targetPos;
 }
 
-// ====== Arm sliders ======
+// Arm sliders
 // Left shoulder (normal)
 BLYNK_WRITE(V7) { servo1.write(param.asInt()); }           // Vertical
 BLYNK_WRITE(V6) { servo2.write(180 - param.asInt()); }     // Horizontal inverted for inward/outward
@@ -74,7 +243,7 @@ BLYNK_WRITE(V12) {
   }
 }
 
-// ====== Motor Setup ======
+// Motor Setup
 const int motorForwardPins[4]  = {12, 27, 16, 21};
 const int motorBackwardPins[4] = {14, 26, 17, 23};
 const int motorEnablePins[4]   = {13, 25, 33, 32};
